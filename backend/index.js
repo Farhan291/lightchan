025a2515238f34946d0ddd9f73b77fff4364feca
farhan291 <farhan29110511@gmail.com>
2025-07-13 16:56:51 +0530

    added self ping

--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ import express from "express"
 import connectdb from "./utils/connectdb.js";
 import router from "./routes/routes.js";
 import cors from "cors"
+import axios from 'axios';
 
 
 
@@ -16,9 +17,27 @@ app.use(cors({
 }));
 app.use("/api",router)
 
+// self ping to prevent sleep onrender
+const ping = async () => {
+  try{
+    const resp =await axios.get( "https://lightchan.onrender.com/api/ping")
+    console.log(resp.status)
+
+  }catch(error){
+    console.log(error)
+  }
+}
+
+app.get('/ping',(req,res) => {
+    res.status(200).send("ping pong ")
+})
+
+
 
 const PORT=process.env.PORT
 app.listen(PORT, () => {
-    console.log(`server started at port ${PORT}`)
-    connectdb()
+    console.log(`server started at port ${PORT}`);
+    connectdb();
+    ping();
+    setInterval(ping,5*60*1000)
 })
